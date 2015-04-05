3e3f153b15491c2f6f338ac08de36447d0ec4165
blacroix <benjlacroix@gmail.com>
2015-04-05 20:53:24 +0200

    remove unused dependencies and add mysql

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -11,8 +10,6 @@ var images = require('./routes/images');
 
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
